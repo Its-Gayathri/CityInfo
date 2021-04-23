67ef7caded43231a1522e713d3c3364858f45c5d
Gayathri U <gayathri.umesh@saggezza.com>
2021-04-23 20:44:23 +0530

    Added PointOfInterest class to the Model

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -17,19 +17,64 @@ namespace CityInfo.API
                {
                    Id = 1,
                    Name ="New York",
-                   Description = "The one with big park"
+                   Description = "The one with big park",
+                   PointsOfInterest = new List<PointOfInterestDto>()
+                   {
+                       new PointOfInterestDto()
+                       {
+                           Id = 1,
+                           Name ="Central Park",
+                           Description = "The most visited Park"
+                       },
+                       new PointOfInterestDto()
+                       {
+                           Id = 2,
+                           Name ="Empire State Building",
+                           Description = "A 102-story skyscrapper"
+                       }
+                   }
                },
                new CityDto()
                {
                    Id =2 ,
                    Name = "Antwerp",
-                   Description = "The one that was never finished"
+                   Description = "The one that was never finished",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                   {
+                       new PointOfInterestDto()
+                       {
+                           Id = 3,
+                           Name ="Cathedral of our Lady",
+                           Description = "A Gothic style cathedral"
+                       },
+                       new PointOfInterestDto()
+                       {
+                           Id = 4,
+                           Name ="Antwerp central station",
+                           Description = "Railway Station"
+                       }
+                   }
                },
                new CityDto()
                {
                    Id = 3,
                    Name = "Paris",
-                   Description = "The one with the Eiffel Tower"
+                   Description = "The one with the Eiffel Tower",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                   {
+                       new PointOfInterestDto()
+                       {
+                           Id = 5,
+                           Name ="Eiffel Tower",
+                           Description = "Iron lattice tower"
+                       },
+                       new PointOfInterestDto()
+                       {
+                           Id = 6,
+                           Name ="The Louvre",
+                           Description = "The world's largest Museum"
+                       }
+                   }
                }      
             };
         }
