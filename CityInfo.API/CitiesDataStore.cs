1ea0c2ed9e5b487fe0515b2e55692b3ebaac3370
Gayathri U <gayathri.umesh@saggezza.com>
2021-04-23 19:57:26 +0530

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,37 @@
+﻿using CityInfo.API.Models;
+using System.Collections.Generic;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();//can work on the same instance till we restart web server
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            //init dummy data
+            Cities = new List<CityDto>()
+            {
+               new CityDto()
+               {
+                   Id = 1,
+                   Name ="New York",
+                   Description = "The one with big park"
+               },
+               new CityDto()
+               {
+                   Id =2 ,
+                   Name = "Antwerp",
+                   Description = "The one that was never finished"
+               },
+               new CityDto()
+               {
+                   Id = 3,
+                   Name = "Paris",
+                   Description = "The one with the Eiffel Tower"
+               }      
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //or [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);               
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id ) //changed JsonResult to IActionResult because we want to show status code along with the returned data
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id);
+            if(cityToReturn == null)
+            {
+                return NotFound();
+            }
+            return Ok(cityToReturn);
+        }
+    }
+}
