5a862663afc2fa716ec5fbf7badbb9e3417c2277
Gayathri U <gayathri.umesh@saggezza.com>
2021-04-25 13:28:14 +0530

    Added Http Post method to create new resource

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
 namespace CityInfo.API.Controllers
@@ -18,7 +19,7 @@ namespace CityInfo.API.Controllers
             //even empty collection is valid in this case(200 Ok), so we will return not found(404) if city == null           
             return Ok(city.PointsOfInterest);
         }
-        [HttpGet("{id}")]
+        [HttpGet("{id}",Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id) //returns only one point of interest from a city
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
@@ -34,5 +35,28 @@ namespace CityInfo.API.Controllers
             }
             return Ok(pointsOfInterest);
         }
+
+        [HttpPost] //POST Point of Interest
+        public IActionResult CreatePointOfInterest(int cityId,[FromBody] PointOfInterestForCreation pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            //demo purpose, to be refactored later 
+            var maxpointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(a => a.PointsOfInterest).Max(p => p.Id);
+
+            var finalpointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxpointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+            city.PointsOfInterest.Add(finalpointOfInterest);
+            return CreatedAtRoute("GetPointOfInterest", new { cityId, id = finalpointOfInterest.Id }, finalpointOfInterest);
+        }
     }
 }
+// [FromBody] attribute is used to specify that the value should be read from the body of the request.
---
+++ CityInfo.API/Models/PointOfInterestForCreation.cs
@@ -0,0 +1,8 @@
+﻿namespace CityInfo.API.Models
+{
+    public class PointOfInterestForCreation
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
